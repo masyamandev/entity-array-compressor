fc5ce21187f8a39b5c7a10fd4d295adf64538ebb
Alexandr Maksymenko <amaksymenko@velti.com>
2016-09-19 15:25:41 +0300

    Slightly better compression on Map due to keys sorting.

--- src/main/java/com/masyaman/datapack/serializers/collections/MapSerializer.java
+++ src/main/java/com/masyaman/datapack/serializers/collections/MapSerializer.java
@@ -1,26 +1,36 @@
 package com.masyaman.datapack.serializers.collections;
 
+import com.masyaman.datapack.cache.ObjectIdCache;
+import com.masyaman.datapack.cache.ObjectIdCacheRingTree;
 import com.masyaman.datapack.reflection.TypeDescriptor;
 import com.masyaman.datapack.serializers.SerializationFactory;
 import com.masyaman.datapack.serializers.Serializer;
 import com.masyaman.datapack.streams.DataWriter;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.*;
 
 class MapSerializer<K, V> implements Serializer<Map<K, V>> {
 
+    public static Integer SORTING_CACHE_SIZE = 0;
+
     private DataWriter os;
-//    private TypeDescriptor<K> keyType;
+    private TypeDescriptor<K> keyType;
     private Serializer<K> keySerializer;
     private Serializer<V> valueSerializer;
 
+    private ObjectIdCache<K> keyUsageTree;
 
     public MapSerializer(DataWriter os, SerializationFactory<K> keySerializationFactory, TypeDescriptor<K> keyType,
                          SerializationFactory<V> valueSerializationFactory, TypeDescriptor<V> valueType) throws IOException {
         this.os = os;
         this.keySerializer = os.createAndRegisterSerializer(keySerializationFactory, keyType);
         this.valueSerializer = os.createAndRegisterSerializer(valueSerializationFactory, valueType);
+        this.keyType = keyType;
+
+        if (SORTING_CACHE_SIZE != null) {
+            this.keyUsageTree = new ObjectIdCacheRingTree<>(SORTING_CACHE_SIZE);
+        }
     }
 
     @Override
@@ -34,21 +44,35 @@ class MapSerializer<K, V> implements Serializer<Map<K, V>> {
 //            keySerializer.serialize(entry.getKey());
 //            valueSerializer.serialize(entry.getValue());
 //        }
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            keySerializer.serialize(entry.getKey());
-        }
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            valueSerializer.serialize(entry.getValue());
-        }
-//        List<K> keys = new ArrayList<>(map.keySet());
+
+//        for (Map.Entry<K, V> entry : map.entrySet()) {
+//            keySerializer.serialize(entry.getKey());
+//        }
+//        for (Map.Entry<K, V> entry : map.entrySet()) {
+//            valueSerializer.serialize(entry.getValue());
+//        }
+
+        List<K> keys = new ArrayList<>(map.keySet());
 //        if (!(map instanceof SortedMap) && (Comparable.class.isAssignableFrom(keyType.getType()))) {
 //            Collections.sort(keys, (Comparator<? super K>) Comparator.naturalOrder());
 //        }
-//        for (K key : keys) {
-//            keySerializer.serialize(key);
-//        }
-//        for (K key : keys) {
-//            valueSerializer.serialize(map.get(key));
-//        }
+        if (keyUsageTree != null) {
+            Collections.sort(keys, (v1, v2) -> Integer.compare(index(v1), index(v2)));
+            for (int i = keys.size() - 1; i >= 0; i--) {
+                keyUsageTree.removeElement(keys.get(i));
+                keyUsageTree.addHead(keys.get(i));
+            }
+        }
+        for (K key : keys) {
+            keySerializer.serialize(key);
+        }
+        for (K key : keys) {
+            valueSerializer.serialize(map.get(key));
+        }
+    }
+
+    private int index(K v1) {
+        int index = keyUsageTree.indexOf(v1);
+        return index >= 0 ? index : Integer.MAX_VALUE;
     }
 }
